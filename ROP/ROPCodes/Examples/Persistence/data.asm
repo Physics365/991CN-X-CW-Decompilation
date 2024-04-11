6e43749a1fdfa33422c57e7760a9d97cfff453ae
fish4terrisa-MSDSM <flyingfish.msdsm@gmail.com>
2024-04-11 09:48:51 +0800

    Persistence/data.asm: Added Compile output and changed data  - 更改了字符串的最后一位，    这样知道哪个地方是数据的结尾。同时也指出了地址的位和写法。

--- ROP/ROPCodes/Examples/Persistence/data.asm
+++ ROP/ROPCodes/Examples/Persistence/data.asm
@@ -1,12 +1,19 @@
 ;124an
 
+;Compiled output:
+;Key Dump Series:
+;C sinh-1 i tanh sinh-1 √ i Arg sinh-1 i Arg <65> tanh √ sinh <65> i Rnd( tanh √ sinh-1 i √ Abs <61> √ i Arg sinh-1 sinh-1 <64> i tanh Rnd( Arg Abs √ i i i i i i i i i i . k 0 1 0 0 <ea> x >a+bi 0 0 0 0 ∟ <03> 2 0
+;Hex Dump:
+;44 6f 20 6e 6f 74 20 67 6f 20 67 65 6e 74 6c 65 20 69 6e 74 6f 20 74 68 61 74 20 67 6f 6f 64 20 6e 69 67 68 74 20 20 20 20 20 20 20 20 20 20 2e e2 30 31 30 30 ea 48 d2 30 30 30 30 c8 03 32 30
+;实际使用时:
+; [48个数字，内容为需要复制的24bytes数据] k 0 1 0 [2bytes的地址,反向写,如0xea30 => 30 ea, 也就是 0 <ea> ] x >a+bi 0 0 0 0 ∟ <03> 2 0
 str:
 
 ;这里放入你要拷贝的48bytes数据
     hex 44 6f 20 6e 6f 74 20 67 6f 20 67 65 6e 74 6c 65 20
     hex 69 6e 74 6f 20 74 68 61 74 20 67 6f 6f 64 20 6e 69 
-    hex 67 68 74 20 20 20 20 20 20 20 20 20 20 20
-;上面的字符为： Do not go gentle into that good night
+    hex 67 68 74 20 20 20 20 20 20 20 20 20 20 2e
+;上面的字符为： Do not go gentle into that good night.
 
 ;    hex 4f 6c 64 20 61 67 65 20 73 68 6f 75 6c 64 20 62 75
 ;    hex 72 6e 20 61 6e 64 20 72 61 76 65 20 61 74 20 63 6c
@@ -14,4 +21,5 @@ str:
 entry:
     pop qr0
     hex 30 ea 48 d2 30 30 30 30  ;0xea30为目标写入的地址，根据需要更改
+;    hex ec ea 48 d2 30 30 30 30
     smart_strcpy
